1719fbfdceb3e9cabb3f2549600fec474d1b1b75
Stefan Geiger <gest01@users.noreply.github.com>
2017-06-15 09:17:28 +0200

    Create hello.js

--- hello.js
+++ hello.js
@@ -1,6 +1,9 @@
 (function () {
 	console.log('removing overlay wrap...');
 	$("#overlay_wrap" ).remove();
+	
+	console.log('removing modal-open...');
+	$("body").removeClass("modal-open")
 })();
 
 
