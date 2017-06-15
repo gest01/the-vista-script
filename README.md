cfe7947ce7108aae6fd2a16ffb33cb7014bae3e2
Stefan Geiger <mail@stefangeiger.ch>
2017-06-15 19:26:31 +0200

    wip

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ URL:
 ```
 javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://raw.githubusercontent.com/gest01/the-vista-script/master/vista.js';})();
 ```
+
+pd : https://rawcdn.githack.com/gest01/the-vista-script/master/vista.js
+dev : https://raw.githack.com/gest01/the-vista-script/master/vista.js
--- vista.js
+++ vista.js
@@ -1,21 +1,34 @@
 (function () {
-	
-	function removeItem(sKey, sPath, sDomain) {
-		console.log('removing cookie...', sKey, sPath, sDomain);
-    		document.cookie = encodeURIComponent(sKey) + 
-                  "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + 
-                  (sDomain ? "; domain=" + sDomain : "") + 
-                  (sPath ? "; path=" + sPath : "");
-}
-	
-	console.log('removing overlay wrap...');
-	$("#overlay_wrap" ).remove();
-	
-	console.log('removing modal-open...');
-	$("body").removeClass("modal-open");
-	
-	
-	removeItem("creid", "/", ".nzz.ch");
+
+	vistaTagi();
+	vistaNzz();
+
+	console.log(navigator);
+	console.log(document);
+
+	function vistaTagi() {
+		if (isHost("tagesanzeiger")) {
+			$("#overlay_wrap").remove();
+			$("body").removeClass("modal-open");
+		}
+	}
+
+	function vistaNzz() {
+		if (isHost("nzz")) {
+			deleteCookie("creid", "/", ".nzz.ch");
+		}
+	}
+
+	function isHost(name) {
+		return document.location.hostname.indexOf(name) > -1;
+	}
+
+	function deleteCookie(sKey, sPath, sDomain) {
+		document.cookie = encodeURIComponent(sKey) +
+			"=; expires=Thu, 01 Jan 1970 00:00:00 GMT" +
+			(sDomain ? "; domain=" + sDomain : "") +
+			(sPath ? "; path=" + sPath : "");
+	}
 
 })();
 
