15190b46730961dab07056915cfa718360dcdcc0
Stefan Geiger <gest01@users.noreply.github.com>
2017-06-15 10:43:37 +0200

    Create hello.js

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
 (function () {
 	
 	function removeItem(sKey, sPath, sDomain) {
+		console.log('removing cookie...', sKey, sPath, sDomain);
     		document.cookie = encodeURIComponent(sKey) + 
                   "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + 
                   (sDomain ? "; domain=" + sDomain : "") + 
